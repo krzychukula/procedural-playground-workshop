237a688650c43df12755702c1a313af3fdfa68d1
Krzysztof Kula <krzychukula@gmail.com>
2014-10-16 17:40:37 +0200

    heights

--- js/playground/main.js
+++ js/playground/main.js
@@ -1,7 +1,10 @@
 var Earthlike = function() {
 
     // We will be using 2048x1024 textures.
-    var textureWidth = 1024, textureHeight = 512;
+    var textureWidth = 2048, textureHeight = 1024;
+
+    var waterLevel = 0.2;
+    var displacementSize = 0.2;
 
     // ProcGen is a helper object we will be using to generate those textures.
     var procgen = ProcGen(textureWidth, textureHeight);
@@ -15,18 +18,45 @@ var Earthlike = function() {
      * place where we'll implement all our logic.
      */
     function generateEarthlikePlanet(randomize) {
-        // the textures we are going to generate will all be returned from here
+        if(randomize){
+          // the textures we are going to generate will all be returned from here
+          var rawHeightMap = procgen.simplexNoise(Math.random()*400, Math.random()*20, 1);
+        }else{
+          // the textures we are going to generate will all be returned from here
+          var rawHeightMap = procgen.simplexNoise(200, 4, 1);
+        }
 
-        var colorMap = procgen.makeRGBMap(function(x,y){
-
-          return rgb(x%256, y%256, 0);
+        var heightMap = procgen.makeFloatMap([rawHeightMap], function(height){
+          if(height < waterLevel){
+            return lerp(height, -1, waterLevel, -1.0, 0);
+          }else{
+            return lerp(height, waterLevel, 1.0, 0, 1.0);
+          }
         })
+
+        var colorMap = procgen.makeRGBMap([heightMap], function(height){
+          if(height < 0){
+            return colorLerp(height, -1.0, 0, rgb(20,20,120), rgb(0, 0, 200));
+          }else{
+            return colorLerp(height, 0, 1.0, rgb(50,150,50), rgb(55, 255, 55));
+          }
+
+        });
+
+        var displacementMap = procgen.makeFloatMap([heightMap], function(height){
+          if(height < 0 ){
+            return 1.0 - height* 0.01;
+          }
+          return 1.0 + displacementSize * height;
+        })
+
+
 
 
         return {
             // we use boring defaults for everything, but we'll write something better soon!
             colorMap: colorMap,//procgen.defaultColorMap(),
-            displacementMap: procgen.defaultDisplacementMap(),
+            displacementMap: displacementMap,
             bumpMap: procgen.defaultBumpMap(),
             lightMap: procgen.defaultLightMap()
         };
