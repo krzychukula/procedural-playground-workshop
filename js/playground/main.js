94820ec72c3080bce3ed81c88f3ed10342e41d42
Krzysztof Kula <krzychukula@gmail.com>
2014-10-16 16:30:14 +0200

    Play with colors :)

--- js/playground/main.js
+++ js/playground/main.js
@@ -1,7 +1,7 @@
 var Earthlike = function() {
 
     // We will be using 2048x1024 textures.
-    var textureWidth = 2048, textureHeight = 1024;
+    var textureWidth = 1024, textureHeight = 512;
 
     // ProcGen is a helper object we will be using to generate those textures.
     var procgen = ProcGen(textureWidth, textureHeight);
@@ -16,9 +16,16 @@ var Earthlike = function() {
      */
     function generateEarthlikePlanet(randomize) {
         // the textures we are going to generate will all be returned from here
+
+        var colorMap = procgen.makeRGBMap(function(x,y){
+
+          return rgb(x%256, y%256, 0);
+        })
+
+
         return {
             // we use boring defaults for everything, but we'll write something better soon!
-            colorMap: procgen.defaultColorMap(),
+            colorMap: colorMap,//procgen.defaultColorMap(),
             displacementMap: procgen.defaultDisplacementMap(),
             bumpMap: procgen.defaultBumpMap(),
             lightMap: procgen.defaultLightMap()
